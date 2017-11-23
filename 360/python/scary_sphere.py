1584bee040c9c693cf1893ebf3737dc3e127addf
Jonathan Hanson <jonathan@jonathan-hanson.org>
2017-11-22 17:23:11 -0800

    cleanup and add unit tests

--- 360/python/scary_sphere.py
+++ 360/python/scary_sphere.py
@@ -1,12 +1,4 @@
-#! /usr/bin/env python3
-
-from math import sqrt
-
 # See: https://projecteuler.net/problem=360
-
-# The radius of the sphere
-r = 45
-# r = 10**10
 
 def is_on_sphere(x, y, z, r):
     """
@@ -18,19 +10,23 @@ def integer_points_on_sphere(r):
     for x in range(-r, r+1):
         for y in range(-r, r+1):
             for z in range(-r, r+1):
-                if (is_on_sphere(x, y, z, r)):
+                if is_on_sphere(x, y, z, r):
                     yield (x, y, z)
 
 
 def manhatten_distance_from_orgin(x, y, z):
     return abs(x) + abs(y) + abs(z)
 
-def main():
+
+def main(r):
     sum = 0
     for x,y,z in integer_points_on_sphere(r):
         sum += manhatten_distance_from_orgin(x, y, z)
-    print("S({}) {}".format(r, sum))
+    return sum
 
 
 if __name__ == "__main__":
-    main()
+    # The radius of the sphere
+    r = 45
+
+    print("S({}) {}".format(r, main(r)))
---
+++ 360/python/test.py
@@ -0,0 +1,16 @@
+import unittest
+
+import scary_sphere
+
+class TestCases(unittest.TestCase):
+    def test_correct_answers(self):
+        correct_answers = {
+            1: 6,
+            45: 34518,
+        }
+        for test_r, test_answer in correct_answers.items():
+            self.assertEqual(scary_sphere.main(test_r), test_answer)
+
+
+if __name__ == '__main__':
+    unittest.main()
