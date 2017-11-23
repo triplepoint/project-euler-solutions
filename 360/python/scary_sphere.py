1fbeb3c9c32de11c5c48cca0eb13c347427912e4
Jonathan Hanson <jonathan@jonathan-hanson.org>
2017-11-22 17:28:53 -0800

    add debug logging

--- 360/python/scary_sphere.py
+++ 360/python/scary_sphere.py
@@ -1,10 +1,16 @@
 # See: https://projecteuler.net/problem=360
 
+import logging, sys
+
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+
 def is_on_sphere(x, y, z, r):
     """
     Is the point given by (x, y, z) on the surface of the sphere of radius r?
     """
     return x**2 + y**2 + z**2 == r**2
+
 
 def integer_points_on_sphere(r):
     for x in range(-r, r+1):
@@ -21,6 +27,7 @@ def manhatten_distance_from_orgin(x, y, z):
 def main(r):
     sum = 0
     for x,y,z in integer_points_on_sphere(r):
+        logging.debug("point on sphere: ({},{},{})".format(x,y,z))
         sum += manhatten_distance_from_orgin(x, y, z)
     return sum
 
